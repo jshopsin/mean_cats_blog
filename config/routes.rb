48bd3678f6b8e4731a484de2160143e236bc0868
Jennifer Shopsin <jshopsin@yahoo.com>
2017-08-27 19:47:26 -0700

    Add controller and views for categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :categories, only: [:index, :show]
+  resources :users, only: [:show]
+  resources :articles, only: [:show, :create]
+
+  root to: "categories#index"
 end
