bfeef4973f6ce4cec765047d4bb94cd2da1d188a
Jennifer Shopsin <jshopsin@yahoo.com>
2017-08-28 13:50:35 -0700

    Fix sessions controller to render on layout view

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,17 +8,6 @@ class User < ApplicationRecord
   validates :username, :email, presence: true, uniqueness: true
   validate :has_password
 
-  include BCrypt
-
-  def password
-    @password ||= Password.new(password_digest)
-  end
-
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_digest = @password
-  end
-
   private
 
   def has_password
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   resources :articles, only: [:show]
 
-  resources :logins, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: "categories#index"
 end
